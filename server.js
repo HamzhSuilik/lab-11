ce66afb34b3416e9af2933590a45a19a46f717a6
HamzhSuilik <9hamzh@gmail.com>
2021-04-06 06:54:23 +0300

    lab-12

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ require('dotenv').config();
 
 //---------------------- Env var -------------------------------
 const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
 //------------------------------------------------------------
 
 // connection
 const express =require('express');
 const cors =require('cors');
 const superagent =require('superagent');
+const pg =require('pg');
+const client = new pg.Client(DATABASE_URL);
 
 const app = express();
 app.use(cors());
@@ -23,10 +26,6 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 //------------------------------------------------------------
 
-// https://www.googleapis.com/books/v1/volumes?
-// intitle --- inauthor
-// https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor
-// https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor
 
 // Creates a new search to the Google Books API
 app.post('/searches', createSearch);
@@ -35,15 +34,21 @@ app.get('/test',(req,res)=>{
     res.render('pages/searches/new.ejs');
 });
 
+let sql_data=[];
+app.get('/main',(req,res)=>{
+  
+  const sqlQuery =`SELECT * FROM saved_book;`;
+  client.query(sqlQuery).then(result =>{
+  }).then(()=>{
+    res.render('pages/index.ejs');
+  })
+    
+});
 
 app.get('*',nothing);
 function nothing (req,res){
     res.status(404).send('you sent Invalid request!!');
 }
-
-app.listen(PORT,()=>{
-    console.log('connect/');
-});
 
 // --------- Functions ---------
 let book_details =[];
@@ -63,7 +68,6 @@ function createSearch(req,res){
     }).catch(err=>{
         res.render('pages/error.ejs', { error: err });
     });
-
     
 }
 
@@ -73,5 +77,25 @@ function Books (data){
     this.title = data.volumeInfo.title ? data.volumeInfo.title : '';
     this.description = data.description ? data.description : '';
     this.author = data.volumeInfo.authors ? data.volumeInfo.authors.join(" - ") : '';
-    this.image = data.volumeInfo.imageLinks.thumbnail ? data.volumeInfo.imageLinks.thumbnail : 'default';  
+    this.image = data.volumeInfo.imageLinks.thumbnail ? data.volumeInfo.imageLinks.thumbnail : 'https://cdn.picpng.com/book/book-view-30965.png'; 
+    this.isbn = 'isbn';
+    this.shelf='shelf';
 }
+
+function Books2 (data){
+    this.title = data.title;
+    this.description = data.description_book;
+    this.author = data.author;
+    this.image = data.image_book; 
+    this.isbn = data.isbn;
+    this.shelf=data.shelf;
+}
+
+// Listen to the port
+
+client.connect().then(()=>{
+    app.listen(PORT,()=>{
+        console.log('connect/');
+    });
+});
+
