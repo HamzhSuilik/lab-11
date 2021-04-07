e57e4f1680aa0cc2227419784fe57825d47e3ac6
HamzhSuilik <9hamzh@gmail.com>
2021-04-07 23:54:49 +0300

    lab12 - trail -2

--- seed.sql
+++ seed.sql
@@ -1,4 +1,5 @@
-INSERT INTO saved_book (title, description_book, image_book, isbn,shelf) 
-VALUES('book1','description_book1','https://cdn.picpng.com/book/book-view-30965.png','isbn1','shelf1'),
- ('book2','description_book2','https://cdn.picpng.com/book/book-view-30965.png','isbn2','shelf2'),
- ('book3','description_book3','https://cdn.picpng.com/book/book-view-30965.png','isbn3','shelf3');
+INSERT INTO saved_book (title, description_book,author, image_book, isbn,shelf) 
+VALUES('book1','description_book1','author1','https://cdn.picpng.com/book/book-view-30965.png','isbn1','shelf1'),
+ ('book2','description_book2','author2','https://cdn.picpng.com/book/book-view-30965.png','isbn2','shelf2'),
+ ('book3','description_book3','author3','https://cdn.picpng.com/book/book-view-30965.png','isbn3','shelf3');
+
